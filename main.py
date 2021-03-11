d20dc68f6236145340805b3e310c56ce95da2cbd
David Fahim <me@davidfahim.com>
2021-03-11 05:04:20 +0000

    Backup 2

--- main.py
+++ main.py
@@ -7,19 +7,28 @@ from dotenv import load_dotenv
 import os
 from multiprocessing import Process
 import sys
+import random
 from flask import Flask, render_template
 
+
 def func1():
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        template_folder='templates',
+        static_folder='static'
+    )
 
     @app.route("/")
     def render():
         with open('data.html', 'r') as filehandle:
             content = filehandle.read()
-        print(content)
-        return render_template('template.html', content=content)
+        return render_template('template.html', content=content), 200
 
-    app.run(debug=True)
+    app.run(
+		host='0.0.0.0',
+		port=8080
+	)
+
 
 def func2():
     driver = webdriver.Firefox()
@@ -67,19 +76,23 @@ def func2():
                     if (started2 == False):
                         continue
             started2 = True
-            driver.get('https://northvilleschools.schoology.com/search/user?page=' + str(x) + '&s=' + i)
+            driver.get(
+                'https://northvilleschools.schoology.com/search/user?page=' +
+                str(x) + '&s=' + i)
             time.sleep(2)
             try:
-                driver.find_elements_by_xpath("//div[@class='results-counter empty']")[0]
+                driver.find_elements_by_xpath(
+                    "//div[@class='results-counter empty']")[0]
                 break
             except:
-                names = driver.find_elements_by_xpath("//a[@class='sExtlink-processed']")
+                names = driver.find_elements_by_xpath(
+                    "//a[@class='sExtlink-processed']")
                 index = 0
                 tempindex1 = ''
                 tempindex2 = ''
                 tempindex3 = ''
                 for n in names:
-                    index+=1
+                    index += 1
                     if (index == 1):
                         tempindex1 = n.get_attribute('innerHTML').split('"')[5]
                     elif (index == 2):
@@ -89,16 +102,21 @@ def func2():
                         if (tempindex2 not in scrapedNames):
                             scrapedNames.append(tempindex2)
                             with open('data.html', 'a') as filehandle:
-                                filehandle.write('<img>' + tempindex1 + '</img><span>' + tempindex2 + '</span><span>' + tempindex3 + '</span><br><br>')
+                                filehandle.write('<img src="' + tempindex1 + '">' +
+                                                 '</img><span>' + tempindex2 +
+                                                 '</span><span>' + tempindex3 +
+                                                 '</span><br><br>')
                             tempindex1 = ''
                             tempindex2 = ''
                             tempindex3 = ''
                             index = 0
             with open('savestate.txt', 'w') as filehandle:
-                filehandle.write(i + '<>' + str(x) + '<>' + ','.join(scrapedNames))
+                filehandle.write(i + '<>' + str(x) + '<>' +
+                                 ','.join(scrapedNames))
 
-if __name__=='__main__':
-    p1 = Process(target = func1)
+
+if __name__ == '__main__':
+    p1 = Process(target=func1)
     p1.start()
-    p2 = Process(target = func2)
+    p2 = Process(target=func2)
     p2.start()
